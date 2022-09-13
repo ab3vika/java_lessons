e52a74851f53d58d447c3200c48886b7e2fdd8d7
ab3vika <113138848+ab3vika@users.noreply.github.com>
2022-09-14 02:13:06 +0300

    Create Fibonacci.java

---
+++ src/lesson2/Fibonacci.java
@@ -0,0 +1,13 @@
+class Fibonacci {
+    public int fib(int n) {
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        if (n > 0) arr[1] = 1;
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) {
+                arr[i] = arr[i - 2] + arr[i - 1];
+            }
+        }
+       return arr[n]; 
+    }
+}
