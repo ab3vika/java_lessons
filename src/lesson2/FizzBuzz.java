97dc4e763c1b06113a2847d6e8656dddcd5b13c4
ab3vika <113138848+ab3vika@users.noreply.github.com>
2022-09-14 17:34:21 +0300

    Create FizzBuzz.java

---
+++ src/lesson2/FizzBuzz.java
@@ -0,0 +1,9 @@
+public class FizzBuzz {
+    public static String fizzBuzz(int n) {
+        String s = "No fizz-buzz";
+        if (n % 3 == 0 && n % 5 == 0) s = "fizz-buzz";
+        else if (n % 3 == 0) s = "fizz";
+        else if (n % 5 == 0) s = "buzz";
+        return s;
+    }
+}
